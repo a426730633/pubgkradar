30b4c72fd770c246556a5a80ab7f05454b084c0c
hezkun <38609632+hezkun@users.noreply.github.com>
2018-04-25 02:30:25 +0800

    Create uppubgk.sh

---
+++ uppubgk.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "一键搭建pubgk雷达"
+echo "你的内网ip" 
+read -p "内网ip： " ip
+cp /root/winnerpubg/restart.sh /root/restart.sh
+chmod +x restart.sh
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+
+echo "ss信息搭建完成，请记住连接信息！"
+read -p "任意键继续......" 
+
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+source ~/.bash_profile
+nvm install v9.8.0
+nvm alias default v9.8.0
+yum -y install gcc-c++
+yum -y install flex
+yum -y install bison
+wget http://www.tcpdump.org/release/libpcap-1.8.1.tar.gz
+tar -zxvf libpcap-1.8.1.tar.gz
+cd libpcap-1.8.1
+./configure
+make
+make install
+
+git clone https://github.com/chaojibinbin/winnerpubg.git
+cd winnerpubg/
+npm i
+npm i -g pino
+npm install -g forever
+forever start index.js sniff eth0 $ip | pino
+
+echo "雷达搭建完成！"
